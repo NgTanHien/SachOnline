9ce3c5c0a5a87f90e477a259f78f90b9fa40b008
NgTanHien <145855208+NgTanHien@users.noreply.github.com>
2023-09-27 13:14:16 +0700

    Add project files.

---
+++ SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// GetChuDe 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }
+        /// <summary>
+        /// GetNhaXuatBan
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult NhaXuatBanPartial()
+        {
+            return PartialView();
+        }
+        /// <summary>
+        /// GetSlider
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+        /// <summary>
+        /// GetSachBanNhieu
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SachBanNhieuPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+    }
+}
